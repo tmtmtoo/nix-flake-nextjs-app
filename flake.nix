d9821dff8145e1ba49c577c555f6cae7ce1a7b30
matsu <tmtmt2209@gmail.com>
2024-06-30 09:28:24 +0000

    chore: enable pnpm

--- flake.nix
+++ flake.nix
@@ -10,12 +10,22 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        corepack = pkgs.stdenv.mkDerivation {
+          name = "corepack";
+          buildInputs = [ pkgs.nodejs ];
+          phases = [ "installPhase" ];
+          installPhase = ''
+            mkdir -p $out/bin
+            corepack enable --install-directory=$out/bin
+          '';
+        };
       in
       {
         devShell = pkgs.mkShell {
           buildInputs = [
             pkgs.nodejs
             pkgs.docker-client
+            corepack
           ];
         };
         formatter = pkgs.nixpkgs-fmt;
