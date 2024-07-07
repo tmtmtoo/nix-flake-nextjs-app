cc626b19164e153062807996feabcd0d98bddf46
matsu <tmtmt2209@gmail.com>
2024-07-07 10:01:50 +0000

    chore: add turbo

--- packages/my-math/index.test.ts
+++ packages/my-math/index.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from "vitest"
 import { pow } from ".";
 
 test("pow", () => {
-    const actual = pow(2)
-    const expected = 4
+    const actual = pow(4)
+    const expected = 16
     expect(actual).toStrictEqual(expected)
 })
